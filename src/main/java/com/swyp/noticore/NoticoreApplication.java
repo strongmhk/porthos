dca0d63b78472b582b44490b38ded264b69f7cd2
jinnieusLab <kitty14904@naver.com>
2025-04-18 14:00:21 +0900

    #1 :sparkles: feature: 프로젝트 초기 세팅
    - 프로젝트 디렉토리 생성
    - 기본 의존성 구성
    - Testcontroller 추가 (/hello)

---
+++ src/main/java/com/swyp/noticore/NoticoreApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.noticore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticoreApplication.class, args);
+	}
+
+}
