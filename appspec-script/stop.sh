32f249226c06a4c1d4afa48b327af35ba762e43c
root <root@i-05603a2da4d61d14e.ap-northeast-2.compute.internal>
2025-04-19 07:31:11 +0000

    feat: update codedeploy init script

--- appspec-script/stop.sh
+++ appspec-script/stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-PID=$(pgrep -f 'noticecore-0.0.1-SNAPSHOT.jar')
+
+# noticecore JAR 종료
+JAR_NAME="noticecore-0.0.1-SNAPSHOT.jar"
+PID=$(pgrep -f $JAR_NAME)
+
 if [ -n "$PID" ]; then
   kill -9 $PID
 fi
