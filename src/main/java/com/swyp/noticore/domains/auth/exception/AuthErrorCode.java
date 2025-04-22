95d5e7cf64e652510fc875f9576cd7605acd095e
strongmhk <kim6562166086@gmail.com>
2025-04-22 16:41:30 +0900

    #21 ✨ feat: 회원, 권한 관련 에러 코드 생성

---
+++ src/main/java/com/swyp/noticore/domains/auth/exception/AuthErrorCode.java
@@ -0,0 +1,22 @@
+package com.swyp.noticore.domains.auth.exception;
+
+import com.swyp.noticore.global.response.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "AUTH_101", "사용자 인증에 실패했습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_102", "유효하지 않은 토큰입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_103", "만료된 토큰입니다."),
+    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_401", "존재하지 않는 토큰입니다."),
+    ;
+
+
+    private final HttpStatus httpStatus;
+    private final String customCode;
+    private final String message;
+}
