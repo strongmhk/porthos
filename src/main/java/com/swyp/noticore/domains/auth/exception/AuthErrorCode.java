eae87a68cfb92cdf629f6ed837c763da658eaf22
strongmhk <kim6562166086@gmail.com>
2025-04-24 17:23:36 +0900

    #21 🐛 fix: global exception handler 예외 정책 변경
    - HTTP 코드만 클라이언트에게 반환하도록 변경

--- src/main/java/com/swyp/noticore/domains/auth/exception/AuthErrorCode.java
+++ src/main/java/com/swyp/noticore/domains/auth/exception/AuthErrorCode.java
@@ -12,7 +12,7 @@ public enum AuthErrorCode implements BaseErrorCode {
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "AUTH_101", "사용자 인증에 실패했습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_102", "유효하지 않은 토큰입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_103", "만료된 토큰입니다."),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_104", "아이디 또는 비밀번호가 일치하지 않습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_104", "이메일 또는 비밀번호가 일치하지 않습니다."),
 
     TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_401", "존재하지 않는 토큰입니다."),
     ;
--- src/main/java/com/swyp/noticore/domains/auth/presentation/AuthController.java
+++ src/main/java/com/swyp/noticore/domains/auth/presentation/AuthController.java
@@ -3,12 +3,12 @@ package com.swyp.noticore.domains.auth.presentation;
 import com.swyp.noticore.domains.auth.application.dto.request.LoginRequest;
 import com.swyp.noticore.domains.auth.application.dto.response.TokenResponse;
 import com.swyp.noticore.domains.auth.application.usecase.AuthUseCase;
-import com.swyp.noticore.global.response.ApplicationResponse;
 import com.swyp.noticore.global.utils.CookieUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +25,7 @@ public class AuthController {
     private static final String REFRESH_KEY = "refreshToken";
 
     @PostMapping("/login")
-    public ApplicationResponse<Void> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response) {
+    public ResponseEntity login(@RequestBody LoginRequest request, HttpServletResponse response) {
         TokenResponse tokenResponse = authUseCase.login(request);
 
         Cookie accessCookie = CookieUtils.createCookie(ACCESS_KEY, tokenResponse.accessToken(), 24 * 60 * 60, true);
@@ -33,6 +33,6 @@ public class AuthController {
         response.addCookie(accessCookie);
         response.addCookie(refreshCookie);
 
-        return ApplicationResponse.onSuccess();
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
