38b4fc9b8aee80fc3f603f25c4379353db72e541
strongmhk <kim6562166086@gmail.com>
2025-05-02 00:43:10 +0900

    #25 ✨ feat: 엔드포인트 관리 Matcher 구현

---
+++ src/main/java/com/swyp/noticore/global/config/security/matcher/RequestMatcherHolder.java
@@ -0,0 +1,73 @@
+package com.swyp.noticore.global.config.security.matcher;
+
+import static com.swyp.noticore.domains.member.domain.constant.Role.*;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+import com.swyp.noticore.domains.member.domain.constant.Role;
+import jakarta.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestMatcherHolder {
+
+    private static final List<RequestInfo> REQUEST_INFO_LIST = List.of(
+
+        // auth
+        new RequestInfo(POST, "/api/auth/login", null),
+        new RequestInfo(POST, "/api/auth/refresh", USER),
+
+        new RequestInfo(GET, "/webjars/**",null),
+
+        // swagger
+        new RequestInfo(GET, "/api-docs/**", null),
+
+        // test
+        new RequestInfo(GET, "/tests/members/**", null),
+
+        // user
+        new RequestInfo(GET, "/api/members/**", USER),
+
+        // static resources
+        new RequestInfo(GET, "/*.ico", null),
+        new RequestInfo(GET, "/resources/**", null),
+        new RequestInfo(GET, "/css/**", null),
+        new RequestInfo(GET, "/js/**", null),
+        new RequestInfo(GET, "/img/**", null),
+
+        // 각 Permission의 권한이 필요한 RequestInfo가 최소 1개씩은 리스트에 있어야함
+        new RequestInfo(GET, "/api/admin/**", ADMIN),
+        new RequestInfo(GET, "/api/super/**", SUPER_ADMIN)
+    );
+    private final ConcurrentHashMap<String, RequestMatcher> reqMatcherCacheMap = new ConcurrentHashMap<>();
+
+    /**
+     * 최소 권한이 주어진 요청에 대한 RequestMatcher 반환
+     * @param minRole 최소 권한 (Nullable)
+     * @return 생성된 RequestMatcher
+     */
+    public RequestMatcher getRequestMatchersByMinPermission(@Nullable Role minRole) {
+        var key = getKeyByRole(minRole);
+        return reqMatcherCacheMap.computeIfAbsent(key, k ->
+            new OrRequestMatcher(REQUEST_INFO_LIST.stream()
+                .filter(reqInfo -> Objects.equals(reqInfo.minRole, minRole))
+                .map(reqInfo -> new AntPathRequestMatcher(reqInfo.pattern(),
+                    reqInfo.method().name()))
+                .toArray(AntPathRequestMatcher[]::new)));
+    }
+
+    private String getKeyByRole(@Nullable Role minRole) {
+        return minRole == null ? "VISITOR" : minRole.name();
+    }
+
+    private record RequestInfo(HttpMethod method, String pattern, Role minRole) {
+
+    }
+}
