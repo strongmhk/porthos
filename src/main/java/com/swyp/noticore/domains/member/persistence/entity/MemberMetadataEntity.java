281274ccf22e508b7e56dc0fa76ed7a12e36743f
jaeho.p <jaeho.p@hanwha.com>
2025-05-07 21:36:12 +0900

    feat: update member crud apis

--- src/main/java/com/swyp/noticore/domains/member/persistence/entity/MemberMetadataEntity.java
+++ src/main/java/com/swyp/noticore/domains/member/persistence/entity/MemberMetadataEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Setter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -15,6 +16,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 @Entity
+@Setter
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
