9204de1dc05ed5ae23fd3886550e377e60d19b0f
jaeho.p <jaeho.p@hanwha.com>
2025-05-08 20:30:58 +0900

    feat: update deploy script

--- appspec-script/start.sh
+++ appspec-script/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "[INFO] Preparing host volumes..."
-mkdir -p /var/porthos/mysql /var/porthos/redis
+echo "[INFO] Restarting Noticore Spring Boot via systemd..."
 
-echo "[INFO] Setting file ownership to ubuntu:ubuntu..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/porthos
-
-
-echo "[INFO] Launching Spring Boot..."
-nohup java -jar /home/ubuntu/porthos/build/libs/noticore-0.0.1-SNAPSHOT.jar > /home/ubuntu/porthos/app.log 2>&1 &
-exit 0
+sudo systemctl restart noticore
