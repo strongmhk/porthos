e98ee6873c5f561849f6710944ac1522deb0489e
strongmhk <kim6562166086@gmail.com>
2025-05-08 11:27:47 +0900

    #37 ✨ feat: 전체 그룹 정보 조회 Service 로직 구현

--- src/main/java/com/swyp/noticore/domains/member/domain/service/GroupMemberQueryService.java
+++ src/main/java/com/swyp/noticore/domains/member/domain/service/GroupMemberQueryService.java
@@ -1,19 +1,20 @@
 package com.swyp.noticore.domains.member.domain.service;
 
+import com.swyp.noticore.domains.member.application.dto.response.GroupInfoResponse;
 import com.swyp.noticore.domains.member.application.dto.response.MemberInfo;
+import com.swyp.noticore.domains.member.persistence.repository.GroupInfoRepository;
 import com.swyp.noticore.domains.member.persistence.repository.MemberGroupRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.swyp.noticore.domains.member.persistence.repository.GroupInfoRepository;
 
 @Slf4j
 @Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
-public class GroupMemberService {
+public class GroupMemberQueryService {
 
     private final MemberGroupRepository memberGroupRepository;
     private final GroupInfoRepository groupInfoRepository;
@@ -23,5 +24,9 @@ public class GroupMemberService {
     }
     public List<String> filterExistingGroupNames(List<String> groupNames) {
         return groupInfoRepository.findNameByNameIn(groupNames);
+    }
+
+    public List<GroupInfoResponse> getAllGroupsInfos() {
+        return memberGroupRepository.findAllGroupInfos();
     }
 }
