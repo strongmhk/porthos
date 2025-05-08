041fbb4fb9226254b5701c8424e613f7ca6975a4
strongmhk <kim6562166086@gmail.com>
2025-05-08 11:26:28 +0900

    #37 ✨ feat: 전체 그룹 정보 조회 Repository 로직 구현

--- src/main/java/com/swyp/noticore/domains/member/persistence/repository/MemberGroupRepository.java
+++ src/main/java/com/swyp/noticore/domains/member/persistence/repository/MemberGroupRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface MemberGroupRepository extends JpaRepository<MemberGroupEntity, Long> {
+public interface MemberGroupRepository extends JpaRepository<MemberGroupEntity, Long>, MemberGroupQueryDslRepository {
 
     @Query("""
         SELECT new com.swyp.noticore.domains.member.application.dto.response.MemberInfo(
