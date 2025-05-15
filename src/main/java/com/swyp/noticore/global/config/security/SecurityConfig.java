f3fc686cdd7bed7e50c770756bd6e599f53a6620
strongmhk <kim6562166086@gmail.com>
2025-05-15 17:02:46 +0900

    #65 🐛 fix: 허용된 origin, headers 수정
    - 서버의 CORS setAllowedOriginPatterns("*")는 setAllowCredentials(true)와 함께 사용 불가능
    - 따라서 정확히 명시

--- src/main/java/com/swyp/noticore/global/config/security/SecurityConfig.java
+++ src/main/java/com/swyp/noticore/global/config/security/SecurityConfig.java
@@ -69,17 +69,22 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        // configuration.setAllowedOrigins(Arrays.asList(
-        //     "http://localhost:3000",
-        //     "http://localhost:8080",
-        //     "http://localhost:5173"
-        // ));
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList(
+             "http://localhost:3000",
+             "http://localhost:8080",
+             "http://localhost:5173"
+        ));
+
+        configuration.setAllowedHeaders(Arrays.asList(
+            "Authorization",
+            "Content-Type",
+            "X-Requested-With"
+        ));
 
         configuration.setAllowedMethods(Arrays.asList(
             "GET",
             "POST",
+            "PUT",
             "DELETE",
             "PATCH",
             "OPTIONS"
